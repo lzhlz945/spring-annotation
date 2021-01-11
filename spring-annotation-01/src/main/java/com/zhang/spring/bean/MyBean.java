dd91336b55bca8c4ede8ece2d94e207f1478fd0b
Administrator <1746678613@qq.com>
2021-01-11 11:31:36 +0800

    bran1:注解开发：BeanPostProcessor在bean初始化前后做什么，生命周期的控制

---
+++ spring-annotation-01/src/main/java/com/zhang/spring/bean/MyBean.java
@@ -0,0 +1,26 @@
+package com.zhang.spring.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: com.zhang.spring.bean
+ * @date:2021/1/6
+ */
+@Component
+public class MyBean implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("初始化之前");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("初始化之后");
+        return bean;
+    }
+}
